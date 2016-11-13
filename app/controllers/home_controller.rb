4f19fece853d9a6688ac17d1c97146f3f7d2d39e
Beadhangerone <denm.2206@gmail.com>
2016-11-13 21:17:36 +0200

    my profile page added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,7 @@ class HomeController < ApplicationController
     layout "application"
   def index
   end
+
+  def my_profile
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
+  
+  get '/profile' => 'home#my_profile'
 
   resources :post
 end
