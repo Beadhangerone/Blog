51940c2216d3bdaa577936cf69afd5038dc7ec2b
Beadhangerone <denm.2206@gmail.com>
2016-11-13 11:54:27 +0200

    initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < ActionController::Base
+  layout "application"
+  protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+end
---
+++ app/controllers/post_controller.rb
@@ -0,0 +1,30 @@
+class PostController < ApplicationController
+  layout "application"
+  before_action :authenticate_user!
+  def index
+    @posts = Post.all.order(created_at: :desc)
+  end
+
+  def new
+    $post=Post.new
+  end
+
+  def create
+    $post.title = params[:post][:title]
+    $post.text = params[:post][:text]
+    $post.author = current_user.username
+    $post.save
+
+    redirect_to post_index_path
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to post_index_path
+  end
+end
