246bd9015747bd2fdc3d3aa5d7ac6a1cbe2c9257
Beadhangerone <denm.2206@gmail.com>
2016-11-14 15:31:54 +0200

    comment display added

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -1,7 +1,7 @@
 class CommentController < ApplicationController
   def create
     post = Post.find(params[:post_id])
-    comment = post.comments.create(comment: params[:comment])
+    comment = post.comments.create(comment: params[:comment], author: current_user.username)
     redirect_to post_path(post)
   end
 
