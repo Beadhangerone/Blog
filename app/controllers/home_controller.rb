fd5fd44e8f531925104de1bce0da26331aff0a38
Beadhangerone <denm.2206@gmail.com>
2016-11-25 19:53:26 +0200

    followings shown in profile

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,11 +6,19 @@ class HomeController < ApplicationController
   def my_profile
     @user = current_user
     @avatar = @user.avatar
-    @posts = Post.where(author_id: @user.id).order(created_at: :desc)
+
     @followers = []
     @user.followers.each do |follower|
       @followers << User.find(follower.follower_id)
     end
+
+    @followings = []
+    @a = current_user.id
+     Follower.where(follower_id: current_user.id).find_each do |following|
+       @followings << User.find(following.follower_id)
+     end
+
+    @posts = Post.where(author_id: @user.id).order(created_at: :desc)
   end
 
   def settings
