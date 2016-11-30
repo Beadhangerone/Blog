6618082699b9645435838a9eae9b5b3b95bb877d
Beadhangerone <denm.2206@gmail.com>
2016-11-30 19:03:04 +0200

    before action get_follow_info added

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,20 +1,10 @@
 class ProfileController < ApplicationController
 
+  before_action :get_folow_info
+
   def show
     @header = "your profile"
-    @user = current_user
     @avatar = @user.avatar
-
-    $followers = []
-    Follower.where(user_id: @user.id).find_each do |follower|
-      $followers << user_by_id(follower.follower_id)
-    end
-
-    $followings = []
-     Follower.where(follower_id: @user.id).find_each do |following|
-       $followings << user_by_id(following.user_id)
-     end
-
     @posts = Post.where(author_id: @user.id).order(created_at: :desc)
   end
 
@@ -26,4 +16,19 @@ class ProfileController < ApplicationController
     @header = "your followings"
   end
 
+  private
+
+  def get_folow_info
+    @user = current_user
+
+    $followers = []
+    Follower.where(user_id: @user.id).find_each do |follower|
+      $followers << user_by_id(follower.follower_id)
+    end
+
+    $followings = []
+     Follower.where(follower_id: @user.id).find_each do |following|
+       $followings << user_by_id(following.user_id)
+     end
+  end
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,21 +1,13 @@
 class UserController < ApplicationController
 
-  before_action :user_by_params_id, except: [:followers, :followings]
+  before_action :user_by_params_id
+  before_action :get_follow_info, except: [:friend_up, :unfollow]
 
   def show
     @header = "#{@user.username}'s page"
     @avatar = @user.avatar
     @posts = Post.where(author_id: @user.id).order(created_at: :desc)
 
-    $followers = []
-    Follower.where(user_id: @user.id).find_each do |follower|
-      $followers << user_by_id(follower.follower_id)
-    end
-
-    $followings = []
-     Follower.where(follower_id: @user.id).find_each do |following|
-       $followings << user_by_id(following.user_id)
-     end
   end
 
   def followers
@@ -40,5 +32,18 @@ class UserController < ApplicationController
 
   def user_by_params_id
     @user = user_by_id(params[:id])
+  end
+
+  def get_follow_info
+
+    $followers = []
+    Follower.where(user_id: @user.id).find_each do |follower|
+      $followers << user_by_id(follower.follower_id)
+    end
+
+    $followings = []
+     Follower.where(follower_id: @user.id).find_each do |following|
+       $followings << user_by_id(following.user_id)
+     end
   end
 end
