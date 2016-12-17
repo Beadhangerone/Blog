e2ffd4a16c5fc1e2c5461655b51069df148f11cd
Beadhangerone <denm.2206@gmail.com>
2016-12-17 17:41:30 +0200

    search form shown when value

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,16 @@
+$(document).ready(function(){
+	if ( $("#search-field").attr("value") ){
+        $("#search-form").show()
+    }
+})
+
+$(document).ready(function(){
+    $("#search-fader").click(function(){
+        $("#search-form").slideToggle(70);
+    });
+});
+
+
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
