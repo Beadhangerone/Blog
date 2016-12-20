602e6ec29ae2e0a014fc627f9f563dfd11431023
Beadhangerone <denm.2206@gmail.com>
2016-12-20 16:41:59 +0200

    registration don't require e-mail anymore

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,17 +5,21 @@ class User < ApplicationRecord
   has_one :avatar
   has_many :followers
 
-  attr_accessor :login
-
-  def self.find_for_database_authentication(warden_conditions)
-    conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
-      where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
-      where(conditions.to_hash).first
-    end
+  def email_required?
+    false
   end
+
+  # attr_accessor :login
+
+  # def self.find_for_database_authentication(warden_conditions)
+  #   conditions = warden_conditions.dup
+  #   if login = conditions.delete(:login)
+  #     where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+  #   elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+  #     where(conditions.to_hash).first
+  #   end
+  # end
  
-  validates :username, uniqueness: {
-  message: "has already been taken" }
+  validates :username, presence: true, uniqueness: {
+  message: "has already been taken" } 
 end
