ff4c46829d2efa55d50c3eabe1031252551e7f79
Beadhangerone <denm.2206@gmail.com>
2016-12-25 13:22:00 +0200

    chevron-change done

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -11,9 +11,25 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
+
     $(".post-text-fader").click(function(){
-    	var target = $($(this).attr("target"))    	
+		var fader = $(this)
+    	var target = $($(this).attr("target"))
+
+		function change_chevron(){
+			if (target.attr("expanded") == 'true'){
+	    		target.attr("expanded", 'false')
+	    		fader.html("<span class='glyphicon glyphicon-chevron-down'></span>")
+	    	}else{
+	    		target.attr("expanded", 'true')
+	    		fader.html("<span class='glyphicon glyphicon-chevron-up'></span>")
+	    	}
+		}
+
+		setTimeout(change_chevron, 750)
         target.slideToggle(700);
+
+
     });
 });
 
