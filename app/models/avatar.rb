160b3d385e316865836829760ed81619c71cae27
Beadhangerone <denm.2206@gmail.com>
2017-01-10 23:28:39 +0200

    avatar validation & active for sign in, sign up

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -3,4 +3,9 @@ class Avatar < ApplicationRecord
   dragonfly_accessor :image
 
   validates :image, presence: true
+  validates_size_of :image, maximum: 500.kilobytes,
+    message: "should not be no more than 500 KB", if: :image_changed?
+
+  validates_property :format, of: :image, in: [:jpeg, :jpg, :png, :bmp], case_sensitive: false,
+    message: "should be either .jpeg, .jpg, .png, .bmp", if: :image_changed?
 end
